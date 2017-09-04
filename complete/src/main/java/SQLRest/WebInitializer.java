78b15c776f08886644e8cd557c223003879ddcbc
pavell00 <mymmo88@gmail.com>
2017-09-04 11:55:15 +0300

    add war type project

---
+++ complete/src/main/java/SQLRest/WebInitializer.java
@@ -0,0 +1,12 @@
+package SQLRest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
