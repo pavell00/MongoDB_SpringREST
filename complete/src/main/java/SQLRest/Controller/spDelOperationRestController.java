ab50ced55de3a2e546b89e27f55b8edd66988ad7
pavel00 <pavel@fromru.com>
2017-10-31 08:48:17 +0300

    add del operation

---
+++ complete/src/main/java/SQLRest/Controller/spDelOperationRestController.java
@@ -0,0 +1,21 @@
+package SQLRest.Controller;
+
+import SQLRest.Repository.StoredProcedureImpl.StoreProcDelOperationImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+
+@RestController
+@CrossOrigin
+public class spDelOperationRestController {
+
+    @Autowired
+    StoreProcDelOperationImpl storeProcDelOperation;
+
+    @RequestMapping(value = "/sp_del_operation", method = RequestMethod.DELETE, headers = "Accept=application/json")
+    public String getPriceListsJSON(@RequestParam("docid") Long docId) throws SQLException {
+
+        return storeProcDelOperation.delOperation(docId);
+    }
+}
