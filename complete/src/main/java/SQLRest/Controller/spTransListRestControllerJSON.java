b13d2e30f8f43d5c9fe6c559aed9ff2b5699476e
pavel00 <pavel@fromru.com>
2017-11-02 16:52:50 +0300

    add transList class

---
+++ complete/src/main/java/SQLRest/Controller/spTransListRestControllerJSON.java
@@ -0,0 +1,23 @@
+package SQLRest.Controller;
+
+import SQLRest.Model.Template;
+import SQLRest.Repository.StoredProcedureImpl.StoreProcTransListRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class spTransListRestControllerJSON {
+
+    @Autowired
+    StoreProcTransListRepositoryImpl storeProcTransListRepository;
+
+    @RequestMapping(value = "/sp_translist", method = RequestMethod.GET, headers = "Accept=application/json")
+    public String getTransList(@RequestParam("docid") Long docId) throws SQLException {
+        String result = storeProcTransListRepository.getTransList_sp(docId);
+        return result;
+    }
+}
---
+++ complete/src/main/java/SQLRest/Repository/StoredProcedureImpl/StoreProcTransListRepositoryImpl.java
@@ -0,0 +1,50 @@
+package SQLRest.Repository.StoredProcedureImpl;
+
+import SQLRest.ApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.session.SessionProperties;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Repository
+public class StoreProcTransListRepositoryImpl {
+    @Autowired
+    private ApplicationConfiguration appConfig;
+    public String getTransList_sp(Long docId) throws SQLException {
+
+        String queryResult = "";
+        Connection con = DriverManager.getConnection(
+                appConfig.appJdbcProp().get("url"),
+                appConfig.appJdbcProp().get("username"),
+                appConfig.appJdbcProp().get("password"));
+        try {
+            CallableStatement proc = con.prepareCall("{ call dbo.sp_transList(?) }");
+            //Задаём входные параметры
+            proc.setLong(1, docId);
+            proc.executeQuery();
+            ResultSet resultSet = proc.getResultSet();
+            while (resultSet.next()){
+                queryResult += resultSet.getString(1);
+            }
+            if (resultSet != null) resultSet.close();
+            if (proc != null) proc.close();
+        } catch (Exception ex) {
+            queryResult = "";
+            Logger.getLogger(SessionProperties.Jdbc.class.getName()).log(Level.SEVERE, "error in " +
+                            "getTransList_sp method, in class - StoreProcTransListRepositoryImpl ",
+                    ex + ", with params docId=" + docId);
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLDataException ex) {
+                    Logger.getLogger(SessionProperties.Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            return queryResult;
+        }
+    }
+}
